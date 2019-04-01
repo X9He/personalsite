db5490ed1a68bb2f23b22816adeb71d1ec2e2409
Evan He <hexianliang9@gmail.com>
2019-04-01 01:29:30 -0400

    changed home background and made some project page changes

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -6,9 +6,9 @@ class project extends Component {
     render() {
         return (
             <div style={{backgroundColor: this.props.projColor}} className='project-container'>
-                <div className="project-left">
-                    <img src={this.props.imgSource} />
-                </div>
+                {/*<div className="project-left">*/}
+                {/*    <img src={this.props.imgSource} />*/}
+                {/*</div>*/}
                 <div className="project-right">
                     <p className='proj-title'>{this.props.title}</p>
                     <p className='proj-description'>{this.props.description}</p>
--- src/components/ProjectHome/ProjectHome.js
+++ src/components/ProjectHome/ProjectHome.js
@@ -8,16 +8,22 @@ class projectHome extends Component {
     render() {
         return (
             <div className='project-background'>
-                <div className='project-root'>
-                    <p className="proj-top-title"> My Side Projects </p>
+                <p className="proj-top-title"> Side Projects </p>
+                <div className="proj-group-title">Web</div>
+                <div className="proj-group">
                     <Project projColor="#7E685A" title="Road Trip Planner (React with Rails and PostgreSQL/GraphQL)" description="You input the locations, starting point and how much time you want to spend at each place,
-                     this app generates the most optimal path for you and all in a nice schedule."/>
-                    <Project projColor="#6F2232" title="Doodle Board (Java)" description="Created a java program using Swing for drawing shapes and lines, where drawings are recorded with in real time and can be played back, skipped
-forward and back, and randomly sought."/>
+                 this app generates the most optimal path for you and all in a nice schedule."/>
                     <Project projColor="#7E685A" title="Web Crawler (Python)" description="Created a web cralwer using python, which returns all embedded urls in
 a web domain specified by a user."/>
+                </div>
+                <div className="divider"/>
+                <div className="proj-group-title">Other</div>
+                <div className="proj-group">
+                    <Project projColor="#6F2232" title="Doodle Board (Java)" description="Created a java program using Swing for drawing shapes and lines, where drawings are recorded with in real time and can be played back, skipped
+forward and back, and randomly sought."/>
                     <Project projColor="#6F2232" title="Dungeon Crawler (C++)" description="Created a bash terminal based dungeon crawling rogue like game with two other teammates, gained valuable OOP and design pattern experience."/>
                 </div>
+                <div className="bottomPadding"/>
             </div>
         );
     }
